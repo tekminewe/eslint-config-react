55ffe2815530ff6b7ea0277f617dc45b5e5877c0
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-06-07 21:37:33 +0100

    feat: add @tanstack/eslint-plugin-query

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
-    'plugin:@typescript-eslint/recommended',
-    "plugin:react-hooks/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   settings: {
     react: {
       version: "detect",
-    }
+    },
   },
   env: {
     browser: true,
     amd: true,
-    node: true
+    node: true,
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', "react"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "@tanstack/query"],
   root: true,
-}
+};
